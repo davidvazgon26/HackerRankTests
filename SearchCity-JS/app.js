c8dd17a09561138dc46f6020b6a27c5d0b46536c
davidvazgon26 <davidvazgon@hotmail.com>
2023-07-15 23:57:33 -0600

    configurando Search City, se levanta server

--- SearchCity-JS/app.js
+++ SearchCity-JS/app.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'view')));
+app.use(express.static(path.join(__dirname, "./view")));
 
-app.listen(process.env.PORT || 8001, '0.0.0.0', function() {});
+app.get("/demo", (req, res) => {
+  res.send("Hola desde app");
+});
+
+app.listen(process.env.PORT || 8001, "0.0.0.0", function () {
+  console.log("Se levanto el servicio en el puerto: 8001");
+});
 module.exports = app;
