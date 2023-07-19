17f6660725650210b0652b7bfafe7f288a7ecabb
David Vazquez <73767511+davidvazgon26@users.noreply.github.com>
2023-07-18 18:42:11 -0600

    Update script.js

--- SearchCity-JS/view/js/script.js
+++ SearchCity-JS/view/js/script.js
@@ -1,4 +1,65 @@
-const boton = document.getElementById("button");
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtener elementos del DOM
+  var input = document.getElementById('input');
+  var button = document.getElementById('button');
+  var resultsTable = document.getElementById('results');
+
+  // click
+  button.addEventListener('click', function() {
+    console.log(input.value)
+    var searchText = input.value.trim();
+
+    if (searchText === "") {
+      alert('Please provide the valid input');
+      return;
+    }
+    var regex = /\s|\d/;
+    console.log(regex.test(searchText))
+    if (regex.test(searchText)) {
+        alert('Please provide the valid input');
+        return;
+    }
+    
+    // API
+    var url = 'https://jsonmock.hackerrank.com/api/cities/?city=' + searchText;
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var cities = data.data;
+        console.log(cities.length);
+        console.log(cities);
+
+        var totalCountElement = document.querySelector('.totalCount');
+        totalCountElement.textContent = 'Total cities found: ' + cities.length;
+
+        //Clear Table
+        resultsTable.innerHTML = '';
+       
+        //Table Cities
+        cities.forEach(function(city) {
+          var row = document.createElement('tr');
+
+          var cityCell = document.createElement('td');
+          cityCell.textContent = city.city;
+          row.appendChild(cityCell);
+
+          var stateCell = document.createElement('td');
+          stateCell.textContent = city.state;
+          row.appendChild(stateCell);
+
+          resultsTable.appendChild(row);
+        });
+      })
+      .catch(function(error) {
+        console.log('Error:', error);
+      });
+  });
+});
+
+
+/*const boton = document.getElementById("button");
 
 boton.addEventListener("click", async () => {
   const input = document.getElementById("input");
@@ -31,6 +92,8 @@ const Search = (text)=>{
         var cities = data.data;
   
 }
+
+*/
 
 /*
 
